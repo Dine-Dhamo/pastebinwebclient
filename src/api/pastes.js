fe824ea0e06efeffab56f53f9a3e51afbf76f75b
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2026-01-29 18:39:16 +0530

    wc finalized code

--- src/api/pastes.js
+++ src/api/pastes.js
@@ -1,7 +1,6 @@
 const BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function createPaste(payload) {
-  console.log(BASE_URL,"cheking the baseurl..")
   const res = await fetch(`${BASE_URL}/api/pastes`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
