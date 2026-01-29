d3eafd02d17d97a660e0564f93fceee789871319
Dineshkumar-DDK <dineshkumarddk98@gmail.com>
2026-01-29 18:14:16 +0530

    console.log check

--- src/api/pastes.js
+++ src/api/pastes.js
@@ -1,6 +1,7 @@
 const BASE_URL = process.env.API_BASE_URL;
 
 export async function createPaste(payload) {
+  console.log(BASE_URL,"cheking the baseurl..")
   const res = await fetch(`${BASE_URL}/api/pastes`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
